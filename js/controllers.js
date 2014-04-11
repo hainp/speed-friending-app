0c275a874c116d8ac4df10b730f9bdae68b81ddf
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-04-11 18:02:12 +0700

    Bypass jshint

--- js/controllers.js
+++ js/controllers.js
@@ -3,8 +3,7 @@
 /* Controllers */
 
 // These lines are to pass jshint
-var angular = angular || null,
-    CONFIG = CONFIG || null;
+var angular = angular || null;
 
 angular.module('myApp.controllers', []).
     controller('HomeCtrl', ['$scope', '$location', '$http', HomeCtrl]);
