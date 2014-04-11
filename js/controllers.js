719dea611a19c4ead52e583c3f6f02fa87ceba52
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-04-11 18:03:37 +0700

    Prototype HomeCtrl

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,37 @@
 var angular = angular || null;
 
 angular.module('myApp.controllers', []).
-    controller('HomeCtrl', ['$scope', '$location', '$http', HomeCtrl]);
+    controller('HomeCtrl', ['$scope', HomeCtrl]);
 
-function HomeCtrl($scope, $location, $http) { }
+function HomeCtrl($scope) {
+    $scope.members = [];
+    $scope.member = {};
+    $scope.lovers = [];
+
+    $scope.add = function() {
+        $scope.member.lurves = $scope.member.lurves.split(',').map(function(l) {
+            return Number(l);
+        });
+        $scope.member.likes = $scope.member.likes.split(',').map(function(l) {
+            return Number(l);
+        });
+        $scope.members.push($scope.member);
+        $scope.member = {};
+    };
+
+    $scope.newData = function() {
+        $scope.members = [];
+        $scope.member = {};
+    };
+
+    $scope.exportData = function() {
+        console.log('Export Function');
+    };
+
+    $scope.matchData = function() {
+        console.log('Match Function');
+    };
+
+    this.matchLovers = function() {
+    };
+}
